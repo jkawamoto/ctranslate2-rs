ba955bdad2480bceebf3025b93ca1293ab3b9b77
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-06-18 21:41:46 -0600

    feat: implement translate_batch

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,17 @@
 use std::ffi::CString;
 use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
+use std::slice::Iter;
 
 use anyhow::{anyhow, bail, Error, Result};
 use tokenizers::{Decoder, EncodeInput, Tokenizer};
 
-use ctranslate2_sys::{release_translation_result, TranslationResult};
+use ctranslate2_sys::{
+    release_translation_result, release_translation_result_array, TranslationResult,
+    TranslationResultArray,
+};
 
-use crate::array::{StringArrayAux, StringArrayPtr};
+use crate::array::{StringArrayArrayAux, StringArrayPtr};
 
 mod array;
 
@@ -62,6 +66,37 @@ impl Drop for TranslationResultPtr {
     }
 }
 
+struct TranslationResultArrayPtr(*const TranslationResultArray, Vec<TranslationResultPtr>);
+
+impl TryFrom<*const TranslationResultArray> for TranslationResultArrayPtr {
+    type Error = Error;
+
+    fn try_from(ptr: *const TranslationResultArray) -> Result<Self> {
+        if ptr.is_null() {
+            bail!("empty result")
+        }
+
+        let mut v = Vec::new();
+        for r in unsafe { std::slice::from_raw_parts((*ptr).arrays, (*ptr).length as usize) } {
+            v.push(TranslationResultPtr::try_from(*r)?)
+        }
+
+        Ok(Self(ptr, v))
+    }
+}
+
+impl TranslationResultArrayPtr {
+    fn iter(&self) -> Iter<'_, TranslationResultPtr> {
+        self.1.iter()
+    }
+}
+
+impl Drop for TranslationResultArrayPtr {
+    fn drop(&mut self) {
+        unsafe { release_translation_result_array(self.0) }
+    }
+}
+
 pub struct Translator {
     translator: ctranslate2_sys::Translator,
     tokenizer: Tokenizer,
@@ -81,41 +116,90 @@ impl Translator {
         })
     }
 
-    pub fn translate<'a, T, U>(
+    // pub fn translate<'a, T, U>(
+    //     &self,
+    //     source: T,
+    //     target_prefix: Vec<U>,
+    // ) -> Result<(String, Option<f32>)>
+    // where
+    //     T: Into<EncodeInput<'a>>,
+    //     U: Into<Vec<u8>>,
+    // {
+    //     let source = StringArrayAux::from(
+    //         self.tokenizer
+    //             .encode(source, true)
+    //             .map_err(|err| anyhow!("failed to encode the given input: {err}"))?
+    //             .get_tokens()
+    //             .to_vec(),
+    //     );
+    //     let target_prefix = StringArrayAux::from(target_prefix);
+    //     let res = TranslationResultPtr::try_from(unsafe {
+    //         self.translator
+    //             .translate(source.as_ptr(), target_prefix.as_ptr())
+    //     })?;
+    //     Ok((
+    //         self.tokenizer
+    //             .get_decoder()
+    //             .unwrap()
+    //             .decode(
+    //                 res.hypothesis()
+    //                     .to_vec()?
+    //                     .into_iter()
+    //                     .skip(target_prefix.len())
+    //                     .collect::<Vec<_>>(),
+    //             )
+    //             .map_err(|err| anyhow!("failed to decode: {err}"))?,
+    //         res.score(),
+    //     ))
+    // }
+
+    pub fn translate_batch<'a, T, U>(
         &self,
-        source: T,
-        target_prefix: Vec<U>,
-    ) -> Result<(String, Option<f32>)>
+        sources: Vec<T>,
+        target_prefixes: Vec<Vec<U>>,
+    ) -> Result<Vec<(String, Option<f32>)>>
     where
         T: Into<EncodeInput<'a>>,
         U: Into<Vec<u8>>,
     {
-        let source = StringArrayAux::from(
-            self.tokenizer
-                .encode(source, true)
-                .map_err(|err| anyhow!("failed to encode the given input: {err}"))?
-                .get_tokens()
-                .to_vec(),
-        );
-        let target_prefix = StringArrayAux::from(target_prefix);
-        let res = TranslationResultPtr::try_from(unsafe {
+        let mut tokens = Vec::with_capacity(sources.len());
+        for s in sources {
+            tokens.push(
+                self.tokenizer
+                    .encode(s, true)
+                    .map_err(|err| anyhow!("failed to encode the given input: {err}"))?
+                    .get_tokens()
+                    .to_vec(),
+            );
+        }
+        let source = StringArrayArrayAux::from(tokens);
+
+        let target_prefix = StringArrayArrayAux::from(target_prefixes);
+
+        let mut res = Vec::new();
+        for (r, prefix) in TranslationResultArrayPtr::try_from(unsafe {
             self.translator
-                .translate(source.as_ptr(), target_prefix.as_ptr())
-        })?;
-        Ok((
-            self.tokenizer
-                .get_decoder()
-                .unwrap()
-                .decode(
-                    res.hypothesis()
-                        .to_vec()?
-                        .into_iter()
-                        .skip(target_prefix.len())
-                        .collect::<Vec<_>>(),
-                )
-                .map_err(|err| anyhow!("failed to decode: {err}"))?,
-            res.score(),
-        ))
+                .translate_batch(source.as_ptr(), target_prefix.as_ptr())
+        })?
+        .iter()
+        .zip(target_prefix.iter())
+        {
+            res.push((
+                self.tokenizer
+                    .get_decoder()
+                    .unwrap()
+                    .decode(
+                        r.hypothesis()
+                            .to_vec()?
+                            .into_iter()
+                            .skip(prefix.len())
+                            .collect::<Vec<_>>(),
+                    )
+                    .map_err(|err| anyhow!("failed to decode: {err}"))?,
+                r.score(),
+            ));
+        }
+        Ok(res)
     }
 }
 
--- sys/wrapper.h
+++ sys/wrapper.h
@@ -13,12 +13,24 @@ struct StringArray {
 
 void release_string_array(StringArray const *a);
 
+struct StringArrayArray {
+  StringArray const *const *arrays;
+  unsigned long length;
+};
+
 struct TranslationResult {
   StringArray const *hypothesis;
   float const *score;
 };
 
 void release_translation_result(TranslationResult const *r);
+
+struct TranslationResultArray {
+  TranslationResult const *const *arrays;
+  unsigned long length;
+};
+
+void release_translation_result_array(TranslationResultArray const *r);
 
 class Translator {
 private:
@@ -28,6 +40,11 @@ public:
   Translator(char const *model_path);
   ~Translator();
 
-  TranslationResult const *translate(const StringArray &source,
-                                     const StringArray &target_prefix) const;
+  //  TranslationResult const *translate(const StringArray &source,
+  //                                     const StringArray &target_prefix)
+  //                                     const;
+
+  TranslationResultArray const *
+  translate_batch(const StringArrayArray &source,
+                  const StringArrayArray &target_prefix) const;
 };
