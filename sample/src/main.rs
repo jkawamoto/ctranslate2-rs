80e97a2aa466314f6590dd59a69e0c11fda0e5a0
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2023-06-20 23:15:51 -0600

    chore: add sample code

---
+++ sample/src/main.rs
@@ -0,0 +1,32 @@
+// main.rs
+//
+// Copyright (c) 2023 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+
+use std::env;
+
+use anyhow::{anyhow, Result};
+
+use ctranslate2::Translator;
+
+fn main() -> Result<()> {
+    let path = env::args()
+        .nth(1)
+        .ok_or(anyhow!("no model path is given"))?;
+    let t = Translator::new(path)?;
+    let res = t.translate_batch(
+        vec![
+            "Hello world!",
+            "This library provides Rust bindings for CTranslate2.",
+        ],
+        vec![vec!["deu_Latn"], vec!["jpn_Jpan"]],
+    )?;
+    for r in res {
+        println!("{}, (score: {:?})", r.0, r.1);
+    }
+
+    Ok(())
+}
