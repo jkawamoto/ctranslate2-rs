fdccde78f5005cc208e60a98abcf4f5340c22474
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2024-05-08 23:57:31 -0600

    docs: update Marian-MT example

--- examples/marian-mt.rs
+++ examples/marian-mt.rs
@@ -6,25 +6,52 @@
 //
 // http://opensource.org/licenses/mit-license.php
 
+//! Translate a file using Marian-MT models.
+//!
+//! In this example, we will use
+//! the [MarianMT](https://huggingface.co/docs/transformers/model_doc/marian) model
+//! to perform a translation from English to German.
+//! The original Python version of the code can be found in the
+//! [CTranslate2 documentation](https://opennmt.net/CTranslate2/guides/transformers.html#marianmt).
+//!
+//! First, convert the model files with the following command:
+//!
+//! ```bash
+//! pip install -U ctranslate2 huggingface_hub torch transformers
+//!
+//! ct2-transformers-converter --model Helsinki-NLP/opus-mt-en-de --output_dir opus-mt-en-de \
+//!     --copy_files source.spm target.spm
+//! ```
+//!
+//! Note: The above command copies `source.spm` and `target.spm` because they are provided by the
+//! [Helsinki-NLP/opus-mt-en-de](https://huggingface.co/Helsinki-NLP/opus-mt-en-de) repository.
+//! If you prefer to use another repository that offers `tokenizer.json`,
+//! you can copy it using the option `--copy_files tokenizer.json`.
+//!
+//! Create a file named `prompt.txt`, write the sentence you want to translate into it,
+//! and save the file.
+//! Then, execute the sample code below with the following command:
+//!
+//! ```bash
+//! cargo run --example marian-mt -- ./opus-mt-en-de
+//! ```
+//!
+
 use std::fs::File;
 use std::io;
-use std::io::{BufRead, BufReader, BufWriter, stdout, Write};
+use std::io::{BufRead, BufReader};
 use std::time;
 
 use anyhow::Result;
 use clap::Parser;
 
 use ct2rs::config::{Config, Device};
-use ct2rs::sentencepiece::Tokenizer;
 use ct2rs::Translator;
 
-/// Translate a file using Marian-MT model.
+/// Translate a file using Marian-MT models.
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Path to the output file. If not specified, output to stdout.
-    #[arg(short, long, value_name = "FILE")]
-    output: Option<String>,
     /// Path to the file contains prompts.
     #[arg(short, long, value_name = "FILE", default_value = "prompt.txt")]
     prompt: String,
@@ -46,7 +73,8 @@ fn main() -> Result<()> {
     } else {
         Config::default()
     };
-    let t = Translator::with_tokenizer(&args.path, Tokenizer::new(&args.path)?, &cfg)?;
+
+    let t = Translator::new(&args.path, &cfg)?;
 
     let sources = BufReader::new(File::open(args.prompt)?)
         .lines()
@@ -56,14 +84,10 @@ fn main() -> Result<()> {
     let res = t.translate_batch(&sources, &Default::default(), None)?;
     let elapsed = now.elapsed();
 
-    let mut out: BufWriter<Box<dyn Write>> = BufWriter::new(match args.output {
-        None => Box::new(stdout()),
-        Some(p) => Box::new(File::create(p)?),
-    });
     for (r, _) in res {
-        writeln!(out, "{r}")?;
+        println!("{r}");
     }
-    writeln!(out, "Time taken: {:?}", elapsed)?;
+    println!("Time taken: {elapsed:?}");
 
     Ok(())
 }
