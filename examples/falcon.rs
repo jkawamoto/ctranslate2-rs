c5894868ea770f7d91c222407d230259041d9214
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2024-05-09 02:03:55 -0600

    feat: add an example for FALCON

---
+++ examples/falcon.rs
@@ -0,0 +1,108 @@
+// falcon.rs
+//
+// Copyright (c) 2023-2024 Junpei Kawamoto
+//
+// This software is released under the MIT License.
+//
+// http://opensource.org/licenses/mit-license.php
+
+//! Generate text using FALCON models.
+//!
+//! In this example, we will use
+//! the [FALCON](https://huggingface.co/tiiuae/falcon-7b) model
+//! to generate text.
+//!
+//! The original Python version of the code can be found in the
+//! [CTranslate2 documentation](https://opennmt.net/CTranslate2/guides/transformers.html#falcon).
+//!
+//! First, convert the model files with the following command:
+//!
+//! ```bash
+//! pip install -U ctranslate2 huggingface_hub torch transformers
+//!
+//! ct2-transformers-converter --model tiiuae/falcon-7b-instruct --output_dir falcon-7b-instruct \
+//!     --copy_files tokenizer.json
+//! ```
+//!
+//! Note: The above command copies `tokenizer.json` because it is provided by the
+//! [tiiuae/falcon-7b-instruct](https://huggingface.co/tiiuae/falcon-7b-instruct) repository.
+//! If you prefer to use another repository that offers `source.spm` and `target.spm`,
+//! you can copy it using the option `--copy_files source.spm target.spm`.
+//!
+//! Create a file named `prompt.txt`, write the prompt, and save the file.
+//! Then, execute the sample code below with the following command:
+//!
+//! ```bash
+//! cargo run --example falcon -- ./falcon-7b-instruct
+//! ```
+//!
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::time;
+
+use anyhow::Result;
+use clap::Parser;
+
+use ct2rs::{GenerationOptions, Generator};
+use ct2rs::config::{Config, Device};
+
+/// Generate text using FALCON models.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to the file contains prompts.
+    #[arg(short, long, value_name = "FILE", default_value = "prompt.txt")]
+    prompt: String,
+    /// Use CUDA.
+    #[arg(short, long)]
+    cuda: bool,
+    /// Path to the directory that contains model.bin.
+    path: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let cfg = if args.cuda {
+        Config {
+            device: Device::CUDA,
+            device_indices: vec![0],
+            ..Config::default()
+        }
+    } else {
+        Config::default()
+    };
+
+    let g = Generator::new(&args.path, &cfg)?;
+    let prompts =
+        BufReader::new(File::open(args.prompt)?)
+            .lines()
+            .fold(Ok(String::new()), |acc, line| {
+                acc.and_then(|mut acc| {
+                    line.map(|l| {
+                        acc.push_str(&l);
+                        acc
+                    })
+                })
+            })?;
+
+    let now = time::Instant::now();
+    let res = g.generate_batch(
+        &vec![prompts],
+        &GenerationOptions {
+            max_length: 200,
+            sampling_topk: 10,
+            include_prompt_in_result: false,
+            ..GenerationOptions::default()
+        },
+        None,
+    )?;
+    let elapsed = now.elapsed();
+
+    for (r, _) in res {
+        println!("{}", r.join("\n"));
+    }
+    println!("Time taken: {elapsed:?}");
+
+    Ok(())
+}
