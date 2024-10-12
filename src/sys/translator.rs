c02422482095679e7e6876d0bcd00a1cd48ea11f
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2024-10-12 00:09:04 -0600

    fix: revert #74

--- src/sys/translator.rs
+++ src/sys/translator.rs
@@ -566,20 +566,6 @@ impl Debug for Translator {
     }
 }
 
-// Releasing `UniquePtr<Translator>` invokes joining threads.
-// However, on Windows, this causes a deadlock.
-// As a workaround, it is bypassed here.
-// See also https://github.com/jkawamoto/ctranslate2-rs/issues/64
-#[cfg(target_os = "windows")]
-impl Drop for Translator {
-    fn drop(&mut self) {
-        let ptr = std::mem::replace(&mut self.ptr, UniquePtr::null());
-        unsafe {
-            std::ptr::drop_in_place(ptr.into_raw());
-        }
-    }
-}
-
 /// A translation result.
 ///
 /// This struct is a Rust binding to the
